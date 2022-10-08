e4da5945c6faaac3d6d657f274c97f2593f9cb5b
edmundwsy <edmundwsy@outlook.com>
2022-10-08 12:31:11 +0200

    🐛 use multi-threading to booster efficiency

--- plan_env/src/test_dsp_map_node.cpp
+++ plan_env/src/test_dsp_map_node.cpp
@@ -12,6 +12,7 @@
 #include <geometry_msgs/PointStamped.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <plan_env/dsp_map_new.h>
+// #include <plan_env/dsp_map.h>
 #include <ros/ros.h>
 #include <sensor_msgs/PointCloud2.h>
 #include <visualization_msgs/Marker.h>
@@ -25,12 +26,14 @@
 #include <pcl/point_cloud.h>
 #include <pcl_conversions/pcl_conversions.h>
 /** include from grid_map */
+#include <geometry_msgs/PoseStamped.h>
 #include <message_filters/subscriber.h>
 #include <message_filters/sync_policies/approximate_time.h>
 #include <message_filters/sync_policies/exact_time.h>
 #include <message_filters/time_synchronizer.h>
 #include <nav_msgs/Odometry.h>
 
+/* ROS Utilities */
 ros::Subscriber click_sub_;
 ros::Publisher  cmd_pub_;
 ros::Publisher  cloud_pub_;
@@ -45,6 +48,7 @@ double          vel_ = 1.0;  // m/s
 double          dt_  = 0.1;  // s
 
 /** sync topics */
+bool is_pose_sub_ = false;
 typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::PointCloud2,
                                                         nav_msgs::Odometry>
     SyncPolicyCloudOdom;
@@ -54,13 +58,17 @@ typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::PointCloud2
 typedef std::shared_ptr<message_filters::Synchronizer<SyncPolicyCloudPose>> SynchronizerCloudPose;
 typedef std::shared_ptr<message_filters::Synchronizer<SyncPolicyCloudOdom>> SynchronizerCloudOdom;
 
-std::shared_ptr<message_filters::Subscriber<nav_msgs::Odometry>>       odom_sub_;
-std::shared_ptr<message_filters::Subscriber<sensor_msgs::PointCloud2>> cloud_sub_;
+std::shared_ptr<message_filters::Subscriber<nav_msgs::Odometry>>         odom_sub_;
+std::shared_ptr<message_filters::Subscriber<geometry_msgs::PoseStamped>> pose_sub_;
+std::shared_ptr<message_filters::Subscriber<sensor_msgs::PointCloud2>>   cloud_sub_;
 
 SynchronizerCloudOdom sync_cloud_odom_;
+SynchronizerCloudPose sync_cloud_pose_;
 
+/* Functions */
 inline bool inRange(const Eigen::Vector3f &p) {
-  if (p.x() > 0.f && p.x() < 6.5f && p.y() > 0.f && p.y() < 6.5f && p.z() > 0.f && p.z() < 6.5f) {
+  if (p.x() > -6.5F && p.x() < 6.5F && p.y() > -6.5F && p.y() < 6.5F && p.z() > -6.5F &&
+      p.z() < 6.5F) {
     return true;
   }
   return false;
@@ -108,8 +116,8 @@ void moveCallback(const ros::TimerEvent &event) {
  *
  */
 void publishMap() {
-  int num_occupied = 0;
-
+  int                            num_occupied = 0;
+  clock_t                        t1           = clock();
   pcl::PointCloud<pcl::PointXYZ> cloud;
   dsp_map_->getOccupancyMapWithRiskMaps(num_occupied, cloud, &risk_maps_[0][0], 0.2);
   for (auto &p : cloud.points) {
@@ -122,17 +130,83 @@ void publishMap() {
   cloud_msg.header.stamp    = ros::Time::now();
   cloud_msg.header.frame_id = "world";
   cloud_pub_.publish(cloud_msg);
+  clock_t t2 = clock();
   std::cout << "num_occupied: " << num_occupied << std::endl;
+  std::cout << "time: " << (t2 - t1) * 1000 / (double)CLOCKS_PER_SEC << "ms" << std::endl;
 }
 
+/**
+ * @brief publish point clouds in a fixed frequency 
+ *
+ * @param event
+ */
 void pubCallback(const ros::TimerEvent &event) { publishMap(); }
 
+/**
+ * @brief subscribe point cloud and odometry
+ *
+ * @param cloud_msg
+ * @param odom_msg
+ */
 void cloudOdomCallback(const sensor_msgs::PointCloud2::ConstPtr &cloud_msg,
                        const nav_msgs::Odometry::ConstPtr &      odom_msg) {
   Eigen::Vector3f    pos(odom_msg->pose.pose.position.x, odom_msg->pose.pose.position.y,
                       odom_msg->pose.pose.position.z);
   Eigen::Quaternionf q(odom_msg->pose.pose.orientation.w, odom_msg->pose.pose.orientation.x,
                        odom_msg->pose.pose.orientation.y, odom_msg->pose.pose.orientation.z);
+  Eigen::Matrix3f    R = q.toRotationMatrix();
+  double             t = cloud_msg->header.stamp.toSec();
+
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_in(new pcl::PointCloud<pcl::PointXYZ>());
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZ>());
+  pcl::fromROSMsg(*cloud_msg, *cloud_in);
+
+  pcl::VoxelGrid<pcl::PointXYZ> sor;
+  sor.setInputCloud(cloud_in);
+  sor.setLeafSize(0.2, 0.2, 0.2);
+  sor.filter(*cloud_filtered);
+
+  /* filter points which are too far from the drone */
+  int n_valid = 0;  // number of valid points
+  for (int i = 0; i < cloud_filtered->points.size(); i++) {
+    float           x = cloud_filtered->points[i].z;
+    float           y = -cloud_filtered->points[i].x;
+    float           z = -cloud_filtered->points[i].y;
+    Eigen::Vector3f p(x, y, z);
+    Eigen::Vector3f p_w = R * p;
+    if (inRange(p)) {
+      valid_clouds_[n_valid * 3 + 0] = x;
+      valid_clouds_[n_valid * 3 + 1] = y;
+      valid_clouds_[n_valid * 3 + 2] = z;
+      n_valid++;
+      if (n_valid >= 5000) {
+        break;
+      }
+    }
+  }
+  clock_t t_update_0 = clock();
+  std::cout << "number of valid points: " << n_valid << std::endl;
+  if (!dsp_map_->update(n_valid, 3, valid_clouds_, pos.x(), pos.y(), pos.z(), t, q.w(), q.x(),
+                        q.y(), q.z())) {
+    return;
+  }
+  clock_t t_update_1 = clock();
+  double  duration   = static_cast<double>((t_update_1 - t_update_0) * 1000 / CLOCKS_PER_SEC);
+  std::cout << "update time: " << duration << "ms" << std::endl;
+}
+
+/**
+ * @brief subscribe point cloud and odometry
+ *
+ * @param cloud_msg
+ * @param pose_msg
+ */
+void cloudPoseCallback(const sensor_msgs::PointCloud2::ConstPtr &  cloud_msg,
+                       const geometry_msgs::PoseStamped::ConstPtr &pose_msg) {
+  Eigen::Vector3f    pos(pose_msg->pose.position.x, pose_msg->pose.position.y,
+                      pose_msg->pose.position.z);
+  Eigen::Quaternionf q(pose_msg->pose.orientation.w, pose_msg->pose.orientation.x,
+                       pose_msg->pose.orientation.y, pose_msg->pose.orientation.z);
   Eigen::Matrix3f    R = q.toRotationMatrix();
   double             t = cloud_msg->header.stamp.toSec();
 
@@ -188,6 +262,7 @@ int main(int argc, char **argv) {
   nh.getParam("pool_size_x", pool_size_x);
   nh.getParam("pool_size_y", pool_size_y);
   nh.getParam("pool_size_z", pool_size_z);
+  nh.getParam("is_pose_sub", is_pose_sub_);
 
   /* initialize map */
   ROS_INFO("init map");
@@ -206,13 +281,22 @@ int main(int argc, char **argv) {
   cur_pos_   = start_pos_;
   click_sub_ = nh.subscribe("/move_base_simple/goal", 1, clickCallback);
 
-  odom_sub_.reset(new message_filters::Subscriber<nav_msgs::Odometry>(
-      nh, "grid_map/odom", 100, ros::TransportHints().tcpNoDelay()));
   cloud_sub_.reset(
       new message_filters::Subscriber<sensor_msgs::PointCloud2>(nh, "grid_map/cloud", 30));
-  sync_cloud_odom_.reset(new message_filters::Synchronizer<SyncPolicyCloudOdom>(
-      SyncPolicyCloudOdom(100), *cloud_sub_, *odom_sub_));
-  sync_cloud_odom_->registerCallback(boost::bind(&cloudOdomCallback, _1, _2));
+  if (!is_pose_sub_) { /* use odometry */
+    odom_sub_.reset(new message_filters::Subscriber<nav_msgs::Odometry>(
+        nh, "grid_map/odom", 100, ros::TransportHints().tcpNoDelay()));
+
+    sync_cloud_odom_.reset(new message_filters::Synchronizer<SyncPolicyCloudOdom>(
+        SyncPolicyCloudOdom(100), *cloud_sub_, *odom_sub_));
+    sync_cloud_odom_->registerCallback(boost::bind(&cloudOdomCallback, _1, _2));
+  } else { /* use pose */
+    pose_sub_.reset(new message_filters::Subscriber<geometry_msgs::PoseStamped>(
+        nh, "grid_map/pose", 100, ros::TransportHints().tcpNoDelay()));
+    sync_cloud_pose_.reset(new message_filters::Synchronizer<SyncPolicyCloudPose>(
+        SyncPolicyCloudPose(100), *cloud_sub_, *pose_sub_));
+    sync_cloud_pose_->registerCallback(boost::bind(&cloudPoseCallback, _1, _2));
+  }
 
   cmd_pub_   = nh.advertise<quadrotor_msgs::PositionCommand>("/pos_command", 1);
   cloud_pub_ = nh.advertise<sensor_msgs::PointCloud2>("grid_map/occupancy_inflated", 1, true);
@@ -220,7 +304,8 @@ int main(int argc, char **argv) {
   ros::Timer move_timer = nh.createTimer(ros::Duration(dt_), moveCallback);
   ros::Timer pub_timer  = nh.createTimer(ros::Duration(0.05), pubCallback);
 
-  ros::spin();
-
+  ros::AsyncSpinner spinner(4);
+  spinner.start();
+  ros::waitForShutdown();
   return 0;
 }
